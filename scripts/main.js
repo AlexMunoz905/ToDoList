11a52aa64e15f7b258f7c3bd91e73a4eb7236c64
Alex Munoz <alex.munoz905@gmail.com>
2017-03-28 06:55:19 -0400

    Added delete using jQuery

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,16 @@ function AddToList() {
   button = document.getElementById('buttonSubmit');
   input = document.getElementById('input').value;
 
-  var para = document.createElement("P");
+  var para = document.createElement("p");
   var t = document.createTextNode(input);
   para.appendChild(t);
   document.body.appendChild(para);
+
+  $(document).ready(function(){
+    $("p").click(function(){
+        $(this).hide();
+    });
+});
 
   // DEBUGING
   // console.log(input);
